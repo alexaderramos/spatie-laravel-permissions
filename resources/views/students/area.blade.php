4ff850114de987462dcdc5ba14cf8a691d4f72a3
Alexander Ramos <tcristian_15.go@hotmail.com>
2020-03-21 03:51:46 -0500

    vistar para los estudiantes (login y home)

---
+++ resources/views/students/area.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.student')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard
+                        <div class="float-right">
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        Welcome Student
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
